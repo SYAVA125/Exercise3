6cbe74ebefb5577457091fca2ec67fb3a47d19f9
SYAVA125 <40748090+SYAVA125@users.noreply.github.com>
2018-07-07 14:40:22 +0900

    Update Dice-Game.c
    ">=" -> ">"   //Not "or more" , It's "more than"

--- Dice-Game.c
+++ Dice-Game.c
@@ -23,7 +23,7 @@ int main() {
   printf("Die 1: %d\n",number1);
   printf("Die 2: %d\n",number2);
   printf("Total value: %d\n",sum);
-  if(sum>=7) printf("%s won!\n",name);
+  if(sum>7) printf("%s won!\n",name);
   else printf("%s lose!\n",name);
   return 0;
 }
